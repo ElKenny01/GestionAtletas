261d8f4a7f4d4243c3b1088eaf2841bca727dad6
Kenny Espinoza <123337013+ElKenny01@users.noreply.github.com>
2025-07-17 14:31:40 -0600

    Add participant assignment via stored procedure

---
+++ App/Data/SIGRAUM2025Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SIGRAUM2025.Models;
+
+namespace SIGRAUM2025.Data
+{
+    public class SIGRAUM2025Context : DbContext
+    {
+        public SIGRAUM2025Context (DbContextOptions<SIGRAUM2025Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SIGRAUM2025.Models.Atleta> Atleta { get; set; } = default!;
+        public DbSet<SIGRAUM2025.Models.RECINTO> RECINTO { get; set; } = default!;
+        public DbSet<SIGRAUM2025.Models.Facultad> Facultad { get; set; } = default!;
+        public DbSet<SIGRAUM2025.Models.ParticipacionEvento> ParticipacionEvento { get; set; } = default!;
+    }
+}
---
+++ App/Models/ParticipacionEvento.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SIGRAUM2025.Models
+{
+    public class ParticipacionEvento
+    {
+
+        [Key]
+        public int IdParticipacionEvento { get; set; }
+
+        [Required]
+        public int IdAtleta { get; set; }
+
+        [Required]
+        public int IdEvento { get; set; }
+
+        [MaxLength(50)]
+        public string? Resultado { get; set; }
+
+        [NotMapped]
+        public string? NombreEvento { get; set; }
+
+    }
+}
